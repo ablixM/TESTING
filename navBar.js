c1286a53e98597300380c8b43309b32326989d4d
AblixM <arehammasresha890@gmail.com>
2024-08-07 09:29:42 +0300

    Update navigation bar styles and logo size

--- navBar.js
+++ navBar.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", () => {
   const navBar = document.querySelector(".nav-bar");
   const navLinks = document.querySelector(".nav-links");
+  const logo = document.querySelector(".logo");
   navBar.classList.toggle("stick", window.scrollY > 0);
   navLinks.classList.toggle("small-nav-link", window.scrollY > 0);
+  logo.classList.toggle("logo-small", window.scrollY > 0);
 });
